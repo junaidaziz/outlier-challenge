661a98c0d7c7400553f511f8c29d094546f36fe3
David <david@outlier.org>
2019-06-14 12:26:08 -0700

    feat: add base64 endpoint

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const tape = require('tape')
 const jsonist = require('jsonist')
 
-const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
+const PORT = (process.env.PORT = process.env.PORT || require('get-PORT-sync')())
 const server = require('./server')
 
 const urlBase = `http://localhost:${PORT}`
 
-tape('should respond hello', (t) => {
+tape('should respond hello', t => {
   jsonist.get(urlBase, (err, body) => {
     if (err) t.error(err)
 
@@ -15,12 +15,21 @@ tape('should respond hello', (t) => {
   })
 })
 
-tape('should respond user-agent', (t) => {
+tape('should respond user-agent', t => {
   const opts = { headers: { 'User-Agent': 'tape' } }
   jsonist.get(`${urlBase}/user-agent`, opts, (err, body) => {
     if (err) t.error(err)
 
     t.equal(body.ua, 'tape')
+    t.end()
+  })
+})
+
+tape('should respond b64', t => {
+  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+    if (err) t.error(err)
+
+    t.equal(body.b64, 'aGVsbG8=')
     t.end()
   })
 })
